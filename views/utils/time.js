db8149b2e37a4f3a74db1145235834441cf0336a
Kliebe, Christian (EXTERN: msg DAVID) <extern.christian.kliebe1@volkswagen.de>
2021-10-25 21:41:25 +0200

    change util time to identify which format is accepted, Fix column header name for parkin lots

--- views/utils/time.js
+++ views/utils/time.js
@@ -1,6 +1,6 @@
-function calculateDaysSinceLetzteAenderung(letzteAenderung) {
-    let year = letzteAenderung.slice(6, 10);
-    let month = letzteAenderung.slice(3, 5);
+function calculateDaysSinceDateDDMMYYYY(letzteAenderung) {
+    let year = letzteAenderung.slice(4, 8);
+    let month = letzteAenderung.slice(2, 4);
     let day = letzteAenderung.slice(0, 2);
     let lastChangeDate = new Date(year, month - 1, day);
     let today = (new Date());
