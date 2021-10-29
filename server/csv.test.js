105fbc17b837e7a49447bfd21dc9f75ab0020a75
Kliebe, Christian (EXTERN: msg DAVID) <extern.christian.kliebe1@volkswagen.de>
2021-10-29 14:35:06 +0200

    Write tests fpr csv.js

---
+++ server/csv.test.js
@@ -0,0 +1,28 @@
+const generateFileContent = require('./csv');
+
+const parkerlaubnis = [{
+    "kennzeichen": "SAW AS 371 E",
+    "land": "U.S.A.",
+    "nachname": "Van Persie",
+    "vorname": "Lisa Maria"
+}]
+
+const expectedCsvRow = 
+    "SAW AS 371 E" + ";" + "U.S.A." + ";" + "Van Persie, " + "Lisa Maria\n" 
+
+test('csv row is longer then given row', () => {
+    expect(generateFileContent(parkerlaubnis).length).toBeGreaterThan("testString".length);
+});
+
+test('csv row ends with \\n to finish as row', () => {
+    let csvRow = generateFileContent(parkerlaubnis);
+    expect(csvRow.substring(csvRow.length -2)).toEqual(expect.stringMatching("\n"));
+});
+
+test('csv row has ; as devider', () => {
+    expect(generateFileContent(parkerlaubnis)).toEqual(expect.stringContaining(";"));
+});
+
+test('csv row looks like this', () => {
+    expect(generateFileContent(parkerlaubnis)).toEqual(expect.stringMatching(expectedCsvRow));
+});
