f8670adb7d1fb97a2e9cbe1f132f9d659a781f0d
Christian <chrstnklb@gmail.com>
2022-05-09 07:01:00 +0200

    First implementation of automatic excel downloads

--- server/ftp-server/cron.js
+++ server/ftp-server/cron.js
@@ -1,75 +1,130 @@
-const { ftpFolderOutgoing, cronTime, dbUrl } = require('../../config.js');
+const { ftpFolderOutgoing, cronTime, dbUrl } = require("../../config.js");
+var excel = require("../excel");
 
-var PouchDB = require('pouchdb');
+var PouchDB = require("pouchdb");
 
 const parkingPlaces = [
-    "A75",
-    "Job Ticket",
-    "P1",
-    "P2",
-    "P3",
-    "P3 Erw",
-    "P4",
-    "P5",
-    "P6",
-    "Werk"
+  "A75",
+  "Job Ticket",
+  "P1",
+  "P2",
+  "P3",
+  "P3 Erw",
+  "P4",
+  "P5",
+  "P6",
+  "Werk",
 ];
 
 let cronDbConnection = new PouchDB(dbUrl);
 
-let CronJob = require('cron').CronJob;
-let job = new CronJob(cronTime, function () {
-    console.log('CRON STARTED');
+let CronJob = require("cron").CronJob;
+let job = new CronJob(
+  cronTime,
+  function () {
+    console.log("CRON STARTED");
     exportAllCameraCsvImportFiles();
-}, null, true, 'America/Los_Angeles');
+    exportAllPermissionsAsExcelFile();
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
 
 let read_db_rows_count = -1;
 let read_db_rows;
 
 function exportAllCameraCsvImportFiles() {
-    getAll()
-    if (dbEntriesSuccessfullyRead()) {
-        parkingPlaces.forEach(parkingPlace => exportCsvForCameraFor(parkingPlace))
-    }
+  getAll();
+  if (dbEntriesSuccessfullyRead()) {
+    parkingPlaces.forEach((parkingPlace) =>
+      exportCsvForCameraFor(parkingPlace)
+    );
+  }
 }
 
 function getAll() {
-    cronDbConnection.allDocs({
-        include_docs: true
-    }).then(function (result) {
-        read_db_rows_count = result.total_rows
-        read_db_rows = result.rows
-    }).catch(function (err) {
-        console.log(err);
+  cronDbConnection
+    .allDocs({
+      include_docs: true,
+    })
+    .then(function (result) {
+      read_db_rows_count = result.total_rows;
+      read_db_rows = result.rows;
+    })
+    .catch(function (err) {
+      console.log(err);
     });
 }
 
 function exportCsvForCameraFor(parking_space) {
-    let data_entry = ""
-    let parking_space_counter = 0
-    read_db_rows = Object.values(read_db_rows);
-    read_db_rows.forEach(element => {
-        if (element.doc["parkplaetze"]?.toString().includes(parking_space)) {
-            parking_space_counter++
-            data_entry = data_entry +
-                element.doc["kennzeichen"].replace("-","") + ";" +
-                element.doc["land"] + ";" +
-                element.doc["nachname"] + ", " +
-                element.doc["vorname"] + "\n"
-        }
-    });
+  let data_entry = "";
+  let parking_space_counter = 0;
+  read_db_rows = Object.values(read_db_rows);
+  read_db_rows.forEach((element) => {
+    if (element.doc["parkplaetze"]?.toString().includes(parking_space)) {
+      parking_space_counter++;
+      data_entry =
+        data_entry +
+        element.doc["kennzeichen"].replace("-", "") +
+        ";" +
+        element.doc["land"] +
+        ";" +
+        element.doc["nachname"] +
+        ", " +
+        element.doc["vorname"] +
+        "\n";
+    }
+  });
 
-    require('./fileWriter')
-        .write(
-            // ftpFolderOutgoing + parking_space + "/" + parking_space,
-            ftpFolderOutgoing + parking_space,
-            "csv",
-            data_entry,
-            "UTF-8",
-            " " + parking_space_counter + " entires to ");
+  require("./fileWriter").write(
+    // ftpFolderOutgoing + parking_space + "/" + parking_space,
+    ftpFolderOutgoing + parking_space,
+    "csv",
+    data_entry,
+    "UTF-8",
+    " " + parking_space_counter + " entires to "
+  );
 }
 
 function dbEntriesSuccessfullyRead() {
-    console.log("Actual read db entry count: " + read_db_rows_count)
-    return read_db_rows_count !== -1;
+  console.log("Actual read db entry count: " + read_db_rows_count);
+  return read_db_rows_count !== -1;
+}
+
+function exportAllPermissionsAsExcelFile() {
+  const timestamp =
+    "_" + new Date(Date.now()).toLocaleDateString() + "_" + Date.now();
+  const filePath = ftpFolderOutgoing + "erlaubnisse" + timestamp + ".xlsx";
+
+  let data = [];
+
+  cronDbConnection.allDocs({ include_docs: true })
+    .then(function (result) {
+      result.rows.forEach((row) => {
+        data.push({
+          Nachname: row.doc.nachname,
+          Vorname: row.doc.vorname,
+          Unternehmen: row.doc.unternehmen,
+          Bereich: row.doc.bereich,
+          Telefon: row.doc.telefon,
+          Kennzeichen: row.doc.kennzeichen,
+          Land: row.doc.land,
+          Fahrzeug: row.doc.fahrzeug,
+          Farbe: row.doc.farbe,
+          Bemerkung: row.doc.bemerkung,
+          Parkplaetze: row.doc.parkplaetze,
+          // explicit: no searchHash as download
+        });
+      });
+    })
+    .then(() => {
+      excel.writeExcelEntriesFromDatabase(data, filePath);
+    })
+    // .then(() => {
+    //   res.download(filePath);
+    // })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
