8005a5b7b842d9faf664be66b22ad3b58e568838
Christian <chrstnklb@gmail.com>
2022-05-13 23:40:39 +0200

    Reduce test to new beginning

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,53 +1,18 @@
-// cy.wait(4000);
-// cy.get("#searchInput").should(
-//   "have.attr",
-//   "placeholder",
-//   " <- Einfach tippen um tausende Parkerlaubnisse nach Kennzeichen, Namen, Parkplätzen und Co. zu durchsuchen."
-// );
+describe("check ui elements in 'result table'", () => {
+  describe("check header ui elements in 'result table'", () => {
+    it("checks header area", () => {
+      cy.visit("http://localhost:3000/");
 
-// cy.contains("Treffer");
+      cy.contains("Neue Parkerlaubnis");
 
-// describe("check ui elements in 'result table'", () => {
-//     describe("check header ui elements in 'result table'", () => {
-//       it("checks button 'Neue Parkerlaubnis'", () => {
-//         cy.contains("Neue Parkerlaubnis");
-//       });
+      cy.wait(4000);
+      cy.get("#searchInput").should(
+        "have.attr",
+        "placeholder",
+        " <- Einfach tippen um tausende Parkerlaubnisse nach Kennzeichen, Namen, Parkplätzen und Co. zu durchsuchen."
+      );
 
-//       it("check column name 'Aktionen'", () => {
-//         cy.contains("Aktionen");
-//       });
-//       it("check column name 'Kennzeichen'", () => {
-//         cy.contains("Kennzeichen");
-//       });
-//       it("check column name 'Land'", () => {
-//         cy.contains("Land");
-//       });
-//       it("check column name 'Bemerkung'", () => {
-//         cy.contains("Bemerkung");
-//       });
-//       it("check column name 'Nachname'", () => {
-//         cy.contains("Nachname");
-//       });
-//       it("check column name 'Vorname'", () => {
-//         cy.contains("Vorname");
-//       });
-//       it("check column name 'Fahrzeug'", () => {
-//         cy.contains("Fahrzeug");
-//       });
-//       it("check column name 'Farbe'", () => {
-//         cy.contains("Farbe");
-//       });
-//       it("check column name 'Unternehmen'", () => {
-//         cy.contains("Unternehmen");
-//       });
-//       it("check column name 'Bereich'", () => {
-//         cy.contains("Bereich");
-//       });
-//       it("check column name 'Telefon'", () => {
-//         cy.contains("Telefon");
-//       });
-//       it("check column name 'Letzte Änderung'", () => {
-//         cy.contains("Letzte Änderung");
-//       });
-//     });
-//   });
+      cy.contains("Treffer");
+    });
+  });
+});
