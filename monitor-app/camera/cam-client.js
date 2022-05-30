ebf8ac11da332210d3a9f705973be7e4cfba8777
Christian <chrstnklb@gmail.com>
2022-05-31 00:21:51 +0200

    clean up cam client from unnecessary code, because it is mostly replaced by tests.

--- monitor-app/camera/cam-client.js
+++ monitor-app/camera/cam-client.js
@@ -1,40 +1,17 @@
 const axios = require('axios').default;
 const time = require('../../server/util/time.js')
 
-const monitorAppUrl = 'http://127.0.0.1:3001';
+function sendPlateToServer(parkplatz, kennzeichen) {
 
+    const URL = "http://127.0.0.1:3001";
 
-
-function callDb() {
-    axios.get(url, {
-        params: { name: 'admin', password: 'admin', }
-    }).then(function (response) {
-        console.log(response);
-    }).catch(function (error) {
-        console.log(error);
-    }).then(function () {// always executed});
-    });
-}
-
-function callServer() {
-    axios.get(monitorAppUrl, {
-        params: '/search' //{ name: 'admin', password: 'admin', }
-    }).then(function (response) {
-        console.log(response);
-    }).catch(function (error) {
-        console.log(error);
-    }).then(function () {// always executed});
-    });
-}
-
-function sendPlateToServer(parkingLot, plate) {
     axios({
         method: 'post',
-        url: monitorAppUrl + '/plate',
+        url: URL + '/plate',
         data: {
-            time: time.getNowAsHH_MM_SS(),
-            parkingLot: parkingLot,
-            plate: plate,
+            zeitpunkt: time.getNowAsHH_MM_SS(),
+            parkplatz: parkplatz,
+            kennzeichen: kennzeichen,
             parkerlaubnisse: 'P1, P2'
         }
     }).then(function (response) {
