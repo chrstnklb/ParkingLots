f1f2bd48ed83afd5d0b398e3f37c035d72e6f4f9
Christian <chrstnklb@gmail.com>
2022-08-04 19:07:57 +0200

    cleanup monitor app server

--- apps/app-monitor/server/monitor-app-server.js
+++ apps/app-monitor/server/monitor-app-server.js
@@ -1,7 +1,5 @@
 const { monitorAppUrl, monitorAppPort } = require('../../../config.js');
-const vorfaelle = require("./vorfaelle.js");
 const createObjectFromJsonFile = require('../../util/json.js').createObjectFromJsonFile;
-const writeObjectToJsonFile = require('../../util/json.js').writeObjectToJsonFile;
 
 const express = require("express");
 const fileUpload = require("express-fileupload");
@@ -16,13 +14,9 @@ monitorApp.use(fileUpload());
 monitorApp.set("view engine", "ejs");
 monitorApp.set("views", "../views");
 
-
 monitorApp.get("/", async function (req, res) {
     console.log("/");
     let readVorfaelle = await createObjectFromJsonFile('../../app-schranke/vorfaelle.json')
-    // readVorfaelle.keys()
-    console.log("🚀 ~ file: monitor-app-server.js ~ line 24 ~ Object.keys(readVorfaelle))", Object.keys(readVorfaelle));
-    console.log("🚀 ~ file: monitor-app-server.js ~ line 23 ~ readVorfaelle", readVorfaelle)
     res.render("monitor", { vorfaelle: readVorfaelle });
 });
 
